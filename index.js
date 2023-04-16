398d73196a33d4b9801d0c1d2eeb56c71ee4c31a
Tugba Esat <tugbaesat007@gmail.com>
2023-04-16 13:15:19 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun(saturdayActivity="roller-skate") {
+    return `This Saturday, I want to ${saturdayActivity}!`;
+}
+function mondayWork(mondayActivity="go to the office") {
+    return `This Monday, I will ${mondayActivity}.`;
+}
+function wrapAdjective(wrapper = "*") {
+    return function innerFunc(innerValue = "special") {
+        return (`You are ${wrapper}${innerValue}${wrapper}!`);
+    }
+}
